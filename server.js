581d76ed95327e8afb19918b8407e3c729115651
Cihan Ozcelik <cihan@artillery.com>
2014-10-17 19:54:16 +0300

    Son hali

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ var app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-
   res.render('index', { title: 'The index page!' })
-
 })
 
 app.get('/kayit', function (req, res) {
-  console.log('helloo basaari')
+  res.render('kayit', { title: 'Bu kayit!' })
+})
+
+app.get('/basari', function (req, res) {
+  res.render('basari', { title: 'Bu basari sayfasi!' })
+})
+
+app.get('/kayit_et', function (req, res) {
+  console.log('Kayit isleri yapiliyor')
   res.redirect('/basari')
 })
 
-
-var server = app.listen(3000, function () {
-
-})
+var server = app.listen(3000);
